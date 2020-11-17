92c312a792ced48dba04af764578e424fa8225b4
Myroslava Nikolaienko <kvi2nya@gmail.com>
2020-11-17 23:59:29 +0200

    Частично решено задание
    Создание и рендер разметки по массиву данных и предоставленному шаблону.
    Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
    Открытие модального окна по клику на элементе галереи.

---
+++ JS/homework.js
@@ -0,0 +1,52 @@
+import galleryItems from '/JS/gallery-items.js'
+
+const creatImagesListRootRef = document.querySelector('.gallery');
+galleryItems.forEach(element => {
+    console.log(element);
+    const listImagesRef = document.createElement(`li`); //создает элемент списка li
+    const linkImagesRef = document.createElement(`a`); //создает элемент a
+    const imagesRef = document.createElement(`img`); //создает элемент списка img
+    imagesRef.insertAdjacentHTML('afterbegin', element);//добавляет элемент в начало списка
+    creatImagesListRootRef.append(listImagesRef);//вставляет узел с элементами li в ul после ('#gallery')
+
+    listImagesRef.classList.add('gallery__item');//добавляет класс
+    
+    listImagesRef.append(linkImagesRef);//вставляет узел с элементами a после li
+    linkImagesRef.classList.add('gallery__link')//добавляет класс
+    linkImagesRef.setAttribute('href', element.original)//добавляет атрибут
+
+    linkImagesRef.append(imagesRef);//вставляет узел с элементами img после a    
+    imagesRef.classList.add('gallery__image')//добавляет класс
+    imagesRef.setAttribute('src', element.preview);//добавляет атрибут
+    imagesRef.setAttribute('alt', element.description);//добавляет атрибут
+    imagesRef.setAttribute('data-source', element.original);//добавляет атрибут        
+}    
+)
+/* const refs = {
+    creatImagesListRootRef: document.querySelector('.gallery');
+    listImagesRef: document.createElement(`li`); //создает элемент списка li
+    linkImagesRef = document.createElement(`a`); //создает элемент a 
+    imagesRef: document.createElement(`img`); //создает элемент списка img
+}
+
+galleryItems.forEach(element => {
+    console.log(element);
+    refs.listImagesRef
+    refs.linkImagesRef
+    refs.imagesRef    
+    imagesRef.insertAdjacentHTML('afterbegin', element);//добавляет элемент в начало списка
+    creatImagesListRootRef.append(listImagesRef);//вставляет узел после ('#gallery')   
+    refs.listImagesRef.append(imagesRef); 
+    imagesRef.classList.add('gallery__image')//добавляет класс
+    imagesRef.setAttribute('src', element.preview);//добавляет атрибут
+    imagesRef.setAttribute('alt', element.description);//добавляет атрибут
+    imagesRef.setAttribute('data-source', element.original);//добавляет атрибут
+    listImagesRef.classList.add('gallery__item');
+} */
+
+
+
+/* creatImagesListRootRef.addEventListener('click', onImageClick)
+function onImageClick (event) {
+
+} */
